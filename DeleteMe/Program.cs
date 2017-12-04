7e392cc005168cb9ef426499f7a30470c6f0fd4d
evans269 <evans269@JB217-L19-W-D>
2017-12-04 15:38:46 -0500

    text changed

--- DeleteMe/Program.cs
+++ DeleteMe/Program.cs
@@ -10,7 +10,7 @@ namespace DeleteMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello NMC!");
 
         }
     }
